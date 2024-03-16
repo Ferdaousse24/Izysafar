74b28706098be3cc710f344f5d935fc76fe8cb9a
safarEasy23 <safar23easy@gmail.com>
2024-03-16 01:05:38 +0100

    Finalisation de l'affichage de menu version 2 et ajout du formulaire version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import {FormComponent} from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
